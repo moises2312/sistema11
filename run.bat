e20f141eb9ab61a8b5c8e8fc5d7e6f1eaf4782d9
Moises Sanchez <mo.sanchez@duocuc.cl>
2024-11-01 02:15:33 -0300

    primera subida

---
+++ run.bat
@@ -0,0 +1,2 @@
+rem ionic cap run android -l --external --host=192.168.100.37 --port=8100 --target=R58T614VCGP
+ionic cap run android -l --external --host=192.168.1.125 --port=8101 --target=R58M548KGNZ
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,28 @@
 import { Routes } from '@angular/router';
+import { loginGuard } from './guards/login.guard';
+import { homeGuard } from './guards/home.guard';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage),
+    canActivate: [loginGuard]
+  },
+  {
+    path: 'map',
+    loadComponent: () => import('./pages/map/map.page').then( m => m.MapPage),
+  },
+  {
+    path: 'theme',
+    loadComponent: () => import('./pages/theme/theme.page').then( m => m.ThemePage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
   },
 ];
