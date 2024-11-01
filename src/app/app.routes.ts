5f044a54665e75d3b23929d26cba3aaa686e2ffe
Moises Sanchez <mo.sanchez@duocuc.cl>
2024-11-01 20:08:17 -0300

    agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { loginGuard } from './guards/login.guard';
 import { homeGuard } from './guards/home.guard';
 
+
 export const routes: Routes = [
   {
     path: '',
@@ -23,6 +24,23 @@ export const routes: Routes = [
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage),
+    canActivate: [homeGuard]
+  },
+  {
+    path: 'correo',
+    loadComponent: () => import('./pages/correo/correo.page').then( m => m.CorreoPage)
+  },
+  {
+    path: 'pregunta',
+    loadComponent: () => import('./pages/pregunta/pregunta.page').then( m => m.PreguntaPage)
+  },
+  {
+    path: 'correcto',
+    loadComponent: () => import('./pages/correcto/correcto.page').then( m => m.CorrectoPage)
+  },
+  {
+    path: 'incorrecto',
+    loadComponent: () => import('./pages/incorrecto/incorrecto.page').then( m => m.IncorrectoPage)
   },
 ];
